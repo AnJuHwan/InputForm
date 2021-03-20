9797f4ab5cb96fee89a4e6c5470f9b3dead49495
wnghks0531 <wnghks0531>
2021-03-20 22:22:54 +0900

    Updates

---
+++ precache-manifest.bb1b7af3ac2d0ba1b8a8593114feb49b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcb9aa0e42e60eedbe4101674cc0e3c5",
+    "url": "/InputForm/index.html"
+  },
+  {
+    "revision": "c3acc792935414e57b37",
+    "url": "/InputForm/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "0213d699771343ed8c09",
+    "url": "/InputForm/static/js/2.4bc100a2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/InputForm/static/js/2.4bc100a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3acc792935414e57b37",
+    "url": "/InputForm/static/js/main.cc47bf65.chunk.js"
+  },
+  {
+    "revision": "ad7582728caf80a83e32",
+    "url": "/InputForm/static/js/runtime-main.7e2cb6a8.js"
+  }
+]);
